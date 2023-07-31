e762889538f01c8a971aa16e6c5405fa565b19fb
Git-HighLanders <sp20-bse-017@cuilahore.edu.pk>
2023-08-01 01:37:48 +0500

    updated helper.js

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-// axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://localhost:8080';
 
 export async function authentication(username) {
     try {
